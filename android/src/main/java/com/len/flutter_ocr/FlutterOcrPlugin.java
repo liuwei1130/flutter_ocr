c1c463097213d53cafe16118efa9acd728827c25
Len <w.liu@holaverse.com>
2019-04-18 14:59:09 +0800

    flutter 插件创建

---
+++ android/src/main/java/com/len/flutter_ocr/FlutterOcrPlugin.java
@@ -0,0 +1,25 @@
+package com.len.flutter_ocr;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterOcrPlugin */
+public class FlutterOcrPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_ocr");
+    channel.setMethodCallHandler(new FlutterOcrPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/FlutterOcrPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterOcrPlugin.h"
+
+@implementation FlutterOcrPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_ocr"
+            binaryMessenger:[registrar messenger]];
+  FlutterOcrPlugin* instance = [[FlutterOcrPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
